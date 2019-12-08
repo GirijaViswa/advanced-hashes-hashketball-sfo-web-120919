3eab2036deea0ffa81abf0fd13a80cc793fd779a
GIRIJA VISWANATHAN <girijavizhwa@gmail.com>
2019-12-08 10:28:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -140,6 +140,7 @@ def most_points_scored
   end
   large_size = arr_val.uniq.sort[-1]
   pp large_size
+  pp new_arr
   new_arr.each do |ele|
     if ele[:points] == large_size
       return ele[:player_namel]
